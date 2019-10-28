2a503d35920ccc91aea34f25b042344ca9916673
abdikeem <abdihakimabdirahman21@gmail.com>
2019-10-28 21:18:28 +0300

    add create quote button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
@@ -21,6 +21,7 @@ import { QuotesFormComponent } from './quotes-form/quotes-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    // FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
