1f56d7c6820fd47bab51b5c31bbdc0c10b775663
abdikeem <abdihakimabdirahman21@gmail.com>
2019-11-24 22:52:33 +0300

    add background and style

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,22 +9,16 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes: Quotes [] = [
-    new Quotes(1, 'Inspirational Quotes', 'If you are working on something that you really care about, you do not have to be pushed',new Date(2020,7,1)),
-    new Quotes(2,'Quotes of the day','It is not whether you get knocked down, is whether you get up',new Date(2019,12,10)),
-    new Quotes(3,'quotes of the year','You learn more from failure than from success',new Date(2024,4,15)),
+    new Quotes(1, 'Inspirational Quote', 'If you are working on something that you really care about, you do not have to be pushed',new Date(2020,7,1)),
+    new Quotes(2,'Quote of the day','It is not whether you get knocked down, is whether you get up',new Date(2019,12,10)),
+    new Quotes(3,'quote of the year','You learn more from failure than from success',new Date(2024,4,15)),
   ];
+
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+    
   }
-  deleteQuotes(isComplete, index){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
-
-      if (toDelete){
-        this.quotes.splice(index,1)
-      }
-    }
-  }
+ 
   
   upVotes:number=0;
     upVote(){
@@ -36,6 +30,16 @@ export class QuotesComponent implements OnInit {
       this.downVotes=this.downVotes+1;
 
     }
+     
+  deleteQuotes(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
 
   addNewGoal(quotes){
     let quotesLength = this.quotes.length;
@@ -43,6 +47,7 @@ export class QuotesComponent implements OnInit {
     quotes.completeDate = new Date(quotes.completeDate)
     this.quotes.push(quotes)
   }
+ 
 
   constructor() { }
 
